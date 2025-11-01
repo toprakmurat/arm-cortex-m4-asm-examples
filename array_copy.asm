9e64ed3143a1af6e3bb8722d8f3af4783e746d06
Murat Toprak <murattoprak2004@gmail.com>
2025-11-01 18:37:11 +0300

    add array_copy.asm file

---
+++ array_copy.asm
@@ -0,0 +1,32 @@
+        AREA ARRAY_COPY, CODE, READONLY    
+        ENTRY                           
+        ALIGN                           
+
+__main  FUNCTION                        
+        EXPORT __main
+
+        LDR     R0, =Source
+        LDR     R1, =Destination
+
+        LDM     R0, {R2 - R5}
+        STM     R1, {R2 - R5}
+
+        B       .
+        ENDFUNC
+
+; -------------------------------
+; Data section
+; -------------------------------
+        AREA SRC_ARRAY, DATA, READONLY
+        EXPORT Source
+        ALIGN
+Source
+        DCD        0x12345678, 0x87654321, 0x11223344, 0x55667788
+
+        AREA DEST_ARRAY, DATA, READWRITE
+        EXPORT Destination
+        ALIGN
+Destination
+        SPACE   16
+
+        END
