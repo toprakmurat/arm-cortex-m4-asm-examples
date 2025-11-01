70f3d0ca9dea8853954cd952e5a4c7232401186c
Murat Toprak <murattoprak2004@gmail.com>
2025-11-01 18:38:07 +0300

    add array_multiplication_single_byte.asm file

---
+++ array_multiplication_single_byte.asm
@@ -0,0 +1,34 @@
+        AREA ARRAY_DIFF, CODE, READONLY    
+        ENTRY                           
+        ALIGN                           
+
+__main  FUNCTION                        
+        EXPORT __main                   
+        LDR         R0, =ArrA
+        LDR         R1, =ArrB
+        LDR         R2, =Result
+        MOV         R3, #4
+        
+loop
+        LDRB        R4, [R0], #1
+        LDRB        R5, [R1], #1
+        MUL         R6, R4, R5
+        STRH        R6, [R2], #2
+        SUBS        R3, R3, #1
+        BNE         loop
+        B           .
+        
+        AREA INPUT_DATA, DATA, READONLY
+        EXPORT ArrA, ArrB
+        ALIGN
+ArrA
+        DCB         0xFF, 0x08, 0x07, 0x06
+ArrB
+        DCB         0xFF, 0x02, 0x03, 0x04
+                
+        AREA RESULT_DATA, DATA, READWRITE
+        EXPORT Result
+        ALIGN
+Result        
+        SPACE       8                  
+        END                             
